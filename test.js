66895e953bf472d92f739732fdc5e19225e467cf
TimIsabella <timjisabella@gmail.com>
2022-07-03 14:41:49 -0700

    All points identified

--- test.js
+++ test.js
@@ -1,17 +1,40 @@
 let spectrumArray = [0, 0, 2, 3, 5, 6, 7, 6, 5, 4, 5, 6, 3, 2, 1, 0, 0, 0, 2, 3, 5, 2, 1, 0, 0];
 
-let i = 0, j = 0;
-let outputString = "";
+let i, j;
+let outputString = "",
+    peakBaseBeginString = "",
+    peakBaseEndString = "",
+    peakTroughString = "",
+    peakString = "";
+    
 
-for(let i = 0; spectrumArray.length > i; i++)
+//Output visual spectrum representation
+for(i = 0; spectrumArray.length > i; i++)
 {
  j = 0;
  outputString = "";
+
+ //Identify peak base begin
+ peakBaseBeginString = "";
+ if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0)) peakBaseBeginString = `       <- Peak base BEGIN: ${spectrumArray[i]} - Element: ${i}`;
+
+ //Identify peak
+ peakString = "";
+ if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) peakString = `       <- PEAK: ${spectrumArray[i]} - Element: ${i}`;
+
+ //Identify peak trough
+ peakTroughString = "";
+ if((spectrumArray[i-1] > spectrumArray[i]) && (spectrumArray[i+1] > spectrumArray[i])) peakTroughString = `       <- Peak TROUGH: ${spectrumArray[i]} - Element: ${i}`;
+
+ //Identify peak base end
+ peakBaseEndString = "";
+ if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) peakBaseEndString = `       <- Peak base END: ${spectrumArray[i]} - Element: ${i}`;
+ 
  while(spectrumArray[i] > j)
  {
   outputString += "-";
   j++;
  }
- console.log(outputString);
+ 
+ console.log(outputString + peakBaseBeginString + peakBaseEndString + peakTroughString, peakString);
 }
-
