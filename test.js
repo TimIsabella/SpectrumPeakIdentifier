20ed6f788ab95f053c6ef62afe7aff2bdef6952f
TimIsabella <timjisabella@gmail.com>
2022-07-03 14:05:49 -0700

    Working basic output representation

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
 let spectrumArray = [0, 0, 2, 3, 5, 6, 7, 6, 5, 4, 5, 6, 3, 2, 1, 0, 0, 0, 2, 3, 5, 2, 1, 0, 0];
+
+let i = 0, j = 0;
+let outputString = "";
+
+for(let i = 0; spectrumArray.length > i; i++)
+{
+ j = 0;
+ outputString = "";
+ while(spectrumArray[i] > j)
+ {
+  outputString += "-";
+  j++;
+ }
+ console.log(outputString);
+}
+
