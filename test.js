76b68357a633d7bc67724e68b651713f4b57a0fc
TimIsabella <timjisabella@gmail.com>
2022-07-04 01:19:16 -0700

    Identifier separated from spectrum

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-let spectrumArray = [0, 0, 2, 4, 7, 6, 10, 9, 8, 5, 4, 9, 12, 6, 0, 0, 0, 0, 1, 2, 0, 3, 8, 12, 20, 22, 20, 12, 7, 4, 3, 2, 0, 1, 2, 6, 7, 4, 3, 2];
+let spectrumArray = [0, 0, 2, 4, 7, 6, 10, 6, 3, 5, 6, 9, 12, 6, 2, 5, 9, 5, 3, 4, 10, 20, 15, 12, 13, 9, 8, 10, 10, 9, 5, 0];
+
+//
+let peakBaseBeginArray = [];
+let peakArray = [];
+let peakTroughArray = [];
+let peakBaseEndArray = [];
 
 let i, j;
 let outputString = "",
@@ -11,34 +17,78 @@ let outputString = "",
 //Output visual spectrum representation
 for(i = 0; spectrumArray.length > i; i++)
 {
- j = 0;
- outputString = "";
+ //Identify peak base begin
+ if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0)) peakBaseBeginArray.push(i);
 
- //Identify peak base begins
- peakBaseBeginString = "";
- if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0)) peakBaseBeginString = `            <- BASE begin (${spectrumArray[i]}) - Element ${i}`;
+ //Identify peak
+ if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) peakArray.push(i);
 
- //Identify peaks
- peakString = "";
- if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) peakString = `          <- PEAK (${spectrumArray[i]}) - Element ${i}`;
+ /*
+ //Identify plateu peak -- when a peak has a flat top
+ if((spectrumArray[i-1] == spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) peakString = `          <- PEAK (${spectrumArray[i]}) - Element ${i}`;
+ */
 
- //Identify peak troughs
- peakTroughString = "";
+ //Identify peak trough
  if((spectrumArray[i-1] > spectrumArray[i]) && (spectrumArray[i+1] > spectrumArray[i]))
    {
 	//Check to ensure dead zones are not counted
-	if(spectrumArray[i] > 0) peakTroughString = `           <- TROUGH (${spectrumArray[i]}) - Element ${i}`;
+	if(spectrumArray[i] > 0) peakTroughArray.push(i);
    }
-
- //Identify peak base ends
- peakBaseEndString = "";
- if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) peakBaseEndString = `          <- BASE end (${spectrumArray[i]}) - Element ${i}`;
  
+ //Identify peak base end
+ if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) peakBaseEndArray.push(i);
+}
+
+
+
+//Output visual spectrum representation
+for(i = 0; spectrumArray.length > i; i++)
+{
+ outputString = "";
+ peakBaseBeginString = "";
+ peakString = "";
+ peakTroughString = "";
+ peakBaseEndString = "";
+ 
+ //Identify peak base begin
+ j = 0;
+ while(peakBaseBeginArray.length > j)
+	 {
+	  if(peakBaseBeginArray[j] == i) peakBaseBeginString = `            <- BASE begin (${spectrumArray[i]}) - Element ${i}`;
+	  j++;
+	 }
+
+ //Identify peak
+ j = 0;
+ while(peakArray.length > j)
+	 {
+	  if(peakArray[j] == i) peakString = `          <- PEAK (${spectrumArray[i]}) - Element ${i}`;
+	  j++;
+	 }
+
+ //Identify peak trough
+ j = 0;
+ while(peakTroughArray.length > j)
+	 {
+	  if(peakTroughArray[j] == i) peakTroughString = `           <- TROUGH (${spectrumArray[i]}) - Element ${i}`;
+	  j++;
+	 }
+
+ //Identify peak base end
+ j = 0;
+ while(peakBaseEndArray.length > j)
+	 {
+	  if(peakBaseEndArray[j] == i) peakBaseEndString = `          <- BASE end (${spectrumArray[i]}) - Element ${i}`;
+	  j++;
+	 }
+ 
+ ///////////Output display///////////
  while(spectrumArray[i] > j)
  {
   outputString += "--";
   j++;
  }
  
+ //Output results to console
  console.log(outputString + peakBaseBeginString + peakBaseEndString + peakTroughString, peakString);
 }
