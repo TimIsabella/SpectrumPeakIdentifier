ced8db3ee1adc010fd4f945be399bb1f5d48ed32
TimIsabella <timjisabella@gmail.com>
2022-07-05 15:47:19 -0700

    Fully working formulas

--- test.js
+++ test.js
@@ -1,32 +1,82 @@
 let spectrumArray = [0, 0, 2, 4, 7, 6, 10, 6, 3, 5, 6, 9, 12, 6, 2, 5, 9, 5, 3, 4, 10, 20, 15, 12, 13, 9, 8, 10, 10, 9, 5, 0];
 
-//
+//let spectrumArray = [0, 1, 2, 3, 66, 3, 2, 1, 2, 3, 25, 3, 2, 1, 1, 1, 1, 1, 1, 2, 3, 11, 3, 2, 1, 0];
+
 let peakBaseBeginArray = [];
-let peakArray = [];
+
+//0 = index, 1 = value
+let peakArray = [[], []];
 let peakTroughArray = [];
 let peakBaseEndArray = [];
+
+let peakAccume = 0;
+let troughAccum = 0;
+
+let weightedPeaks = 0;
+let weightedPeaksDivisor = 0;
+let weightedCenter = 0;
+let peakCenter = 0;
 
 let i, j;
 let outputString = "",
     peakBaseBeginString = "",
     peakBaseEndString = "",
     peakTroughString = "",
-    peakString = "";
+    peakString = "",
+    weightedCenterStr = "",
+    peakCenterStr = "";
     
 
-//TODO - need to add a plateu identifier when a peak has a flat top -- where two adjacent peaks match the same height
+//TODO - need to add a peak plateu identifier -- where two adjacent peaks match the same height
+//TODO - need to add a trough plateu identifier -- where two adjacent troughs match the same height
 
-//Output visual spectrum representation
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//Establish points for spectrum
 for(i = 0; spectrumArray.length > i; i++)
 {
  if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0)) peakBaseBeginArray.push(i); //Identify peak base begin
- if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) peakArray.push(i); //Identify peak
- if((spectrumArray[i-1] > spectrumArray[i]) && (spectrumArray[i+1] > spectrumArray[i])) if(spectrumArray[i] > 0) peakTroughArray.push(i); //Identify peak trough with extra check to ensure dead zones are not counter
+ //if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) {peakArray.push(i); peakAvg += spectrumArray[i];} //Identify peak
+ 
+ //Identify peak
+ if((spectrumArray[i-1] < spectrumArray[i]) && (spectrumArray[i+1] < spectrumArray[i])) 
+   {
+    peakArray[0].push(i);                   //Index
+    peakArray[1].push(spectrumArray[i]);    //Value
+    
+    peakAccume += i;
+   }
+ 
+ //Identify peak trough with extra check to ensure dead zones are not counter
+ if((spectrumArray[i-1] > spectrumArray[i]) && (spectrumArray[i+1] > spectrumArray[i])) if(spectrumArray[i] > 0) 
+   {
+    peakTroughArray.push(i);
+    troughAccum += spectrumArray[i];
+    
+    troughAccum += i;
+   } 
+   
  if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) peakBaseEndArray.push(i); //Identify peak base end
 }
 
 
-//Output visual spectrum representation
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Calculated weighted peak center
+// (∑(peak value * index point)) / ∑(peak value)
+for(i = 0; peakArray[0].length > i; i++)
+{
+ weightedPeaks += peakArray[1][i] * peakArray[0][i];
+ weightedPeaksDivisor += peakArray[1][i];
+}
+
+weightedCenter = Math.round(weightedPeaks / weightedPeaksDivisor);
+peakCenter = Math.round(peakAccume / peakArray[0].length);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Create points for visual spectrum representation
 for(i = 0; spectrumArray.length > i; i++)
 {
  outputString = "";
@@ -34,6 +84,8 @@ for(i = 0; spectrumArray.length > i; i++)
  peakString = "";
  peakTroughString = "";
  peakBaseEndString = "";
+ peakCenterStr = "";
+ weightedCenterStr = "";
  
  //Identify peak base begin
  j = 0;
@@ -45,9 +97,9 @@ for(i = 0; spectrumArray.length > i; i++)
 
  //Identify peak
  j = 0;
- while(peakArray.length > j)
+ while(peakArray[0].length > j)
 	 {
-	  if(peakArray[j] == i) peakString = `          <- PEAK (${spectrumArray[i]}) - Element ${i}`;
+	  if(peakArray[0][j] == i) peakString = `          <- PEAK (${peakArray[1][j]}) - Element ${peakArray[0][j]}`;
 	  j++;
 	 }
 
@@ -67,8 +119,11 @@ for(i = 0; spectrumArray.length > i; i++)
 	  j++;
 	 }
  
+ if(i == peakCenter) peakCenterStr = `          <------ PEAK CENTER (${spectrumArray[i]}) - Element ${i}`;
+ if(i == weightedCenter) weightedCenterStr = `          <------ PEAK WEIGHTED CENTER (${spectrumArray[i]}) - Element ${i}`;
+ 
+ //Output spectrum display bars
  j = 0;
- ///////////Output display///////////
  while(spectrumArray[i] > j)
  {
   outputString += "███";
@@ -76,5 +131,15 @@ for(i = 0; spectrumArray.length > i; i++)
  }
  
  //Output results to console
- console.log(outputString + peakBaseBeginString + peakBaseEndString + peakTroughString, peakString);
+ console.log(outputString + peakBaseBeginString + peakBaseEndString + peakTroughString, peakString, peakCenterStr, weightedCenterStr);
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+console.log("\n\n")
+console.log(`Array Length: ${spectrumArray.length}`);
+console.log(`Peak Average Center Index: ${peakCenter}`);
+//console.log(`Trough Average: ${troughAvg / peakTroughArray.length}`);
+console.log(`Weighted Peak Center Index: ${weightedCenter}`);
+
