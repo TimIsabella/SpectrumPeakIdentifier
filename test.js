e1d703add02f1318d51a33a240fe85ccbbc597c3
TimIsabella <timjisabella@gmail.com>
2022-07-07 15:37:52 -0700

    Refactored without .push() and working

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Node test environment for the creation of formulas which identify both peaks, peak averages, and weighted peak points within a spectrum
+//-- Written to match a separate C-like language which DOES NOT use functions, has very few methods, and only runs procedurally
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //let spectrumArray = [0, 0, 2, 4, 7, 6, 10, 10, 6, 3, 5, 6, 9, 12, 6, 2, 5, 9, 5, 3, 4, 10, 20, 15, 12, 13, 9, 8, 10, 10, 9, 5, 0];
 let spectrumArray = [0, 1, 2, 3, 33, 3, 2, 1, 2, 3, 20, 25, 26, 26, 26, 3, 2, 1, 1, 1, 1, 1, 1, 2, 3, 44, 3, 2, 1, 0];
 //let spectrumArray = [0, 1, 3, 4, 11, 3, 2, 1, 0];
 //let spectrumArray = [0,9,2,8,13,22,19,15,8,18,14,6,9,21,20,18,9,16,2,15,9,16,4,8,3,8,16,8,5,19,4,12,3,10,10,13,8,21,4,1,16,19,3,6,14,21,22,15,19,19,22,19,9,22,5,7,7,14,8,7,2,3,13,19,6,11,2,8,7,14,16,21,22,19,4,5,16,14,12,19,19,8,19,3,12,17,17,13,21,14,12,7,20,18,17,19,15,7,21,21,14,8,2,17,20,14,5,19,17,5,1,15,0];
 
+let spectrumLength = spectrumArray.length;
 
 //0 = index, 1 = value
 let peakArray = [[], []];
@@ -17,7 +23,7 @@ let weightedCenter = 0;
 let peakCenter = 0;
 let peakPlateauCount = 0;
 
-let i, j;
+let i, j, k;
 let outputString = "",
     peakBaseBeginString = "",
     peakBaseEndString = "",
@@ -32,10 +38,24 @@ let outputString = "",
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //Establish points for spectrum
-for(i = 0; spectrumArray.length > i; i++)
+for(i = 0; spectrumLength > i; i++)
 {
  //Identify peak base begin
- if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0)) peakBaseBeginArray.push(i);
+ if((spectrumArray[i-1] == 0) && (spectrumArray[i] > 0))
+   {
+    //Push peak base
+    k = 0;
+    while(true)
+         {
+          if(!peakBaseBeginArray[k])
+            {
+             peakBaseBeginArray[k] = i; //Push index
+             
+             break;
+            }
+          else k++;
+         }
+   }
  
  //Identify peak beginning (lower to higher)
  if(spectrumArray[i-1] < spectrumArray[i]) 
@@ -43,8 +63,20 @@ for(i = 0; spectrumArray.length > i; i++)
     //If peak falling (higher to lower)
     if(spectrumArray[i] > spectrumArray[i+1])
       {
-       peakArray[0].push(i);                   //Index
-       peakArray[1].push(spectrumArray[i]);    //Value
+       
+       //Push peak
+       k = 0;
+       while(true)
+            {
+             if(!peakArray[0][k]) 
+               {
+                peakArray[0][k] = i; //Push index
+                peakArray[1][k] = spectrumArray[i]; //Push value
+                
+                break;
+               }
+             else k++;
+            }
       
        peakAccume += i;
       }
@@ -61,8 +93,21 @@ for(i = 0; spectrumArray.length > i; i++)
              //If plateau end at a fall, plateau complete, push peak point and break loop
              if(spectrumArray[i+j] > spectrumArray[i+1+j])
                {
-                peakArray[0].push(i + Math.round(peakPlateauCount / 2));                   //Index
-                peakArray[1].push(spectrumArray[i + Math.round(peakPlateauCount / 2)]);    //Value
+               
+                //Push peak
+                k = 0;
+                while(true)
+                     {
+                      if(!peakArray[0][k])
+                        {
+                         peakArray[0][k] = i + Math.round(peakPlateauCount / 2); //Push index
+                         peakArray[1][k] = spectrumArray[i + Math.round(peakPlateauCount / 2)]; //Push value
+                         
+                         break;
+                        }
+                      else k++;
+                     }
+                
                 peakAccume += i + Math.round(peakPlateauCount / 2);
                 
                 break;
@@ -78,7 +123,21 @@ for(i = 0; spectrumArray.length > i; i++)
    }
 
  //Identify peak base end
- if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) peakBaseEndArray.push(i);
+ if((spectrumArray[i-1] > 0) && (spectrumArray[i] == 0)) 
+   {
+    //Push peak base
+    k = 0;
+    while(true)
+         {
+          if(!peakBaseEndArray[k])
+            {
+             peakBaseEndArray[k] = i; //Push index
+             
+             break;
+            }
+          else k++;
+         }
+   }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
