1fe5387c8db177983fc44b31847fa7e499330907
TimIsabella <timjisabella@gmail.com>
2022-07-16 23:27:18 -0700

    Drastically refactored and working combined

--- test.js
+++ test.js
@@ -11,9 +11,9 @@
 //let VspecContSR = [11, 9, 6, 3, 0, 3, 6, 9, 11];
 //let VspecContSR = [0,9,2,8,13,22,19,15,8,18,14,6,9,21,20,18,9,16,2,15,9,16,4,0,0,8,3,8,16,8,5,19,4,12,3,10,10,13,8,21,4,1,0,0,16,19,3,6,14,21,22,15,19,19,22,19,9,22,5,7,7,0,0,14,8,7,2,3,13,19,6,11,2,8,7,14,16,21,22,19,4,0,0,5,16,14,12,19,19,8,19,3,0,0,12,17,17,13,21,14,12,7,20,18,17,19,15,7,21,21,14,8,0,0,0,2,17,20,14,5,19,17,5,1,15,0];
 //let VspecContSR = [0, 1, 2, 3, 33, 3, 2, 5, 10, 22, 7, 8, 2, 1, 0, 0, 0, 2, 3, 20, 25, 26, 26, 26, 3, 8, 5, 2, 1, 8, 9, 11, 7, 2, 1, 0, 0, 0, 1, 1, 1, 1, 2, 3, 44, 3, 2, 1, 0];
-//let VspecContSR = [62, 6, 65, 7, 18, 32, 49, 10, 17, 63, 55, 57, 23, 21, 53, 53, 39, 33, 56, 58, 25, 11, 48, 37, 34, 62, 59, 40, 64, 55, 46, 62, 61, 14, 57, 12, 27, 65, 0, 57, 65, 30, 35, 41, 53, 14, 62, 4, 55, 46, 19, 53, 65, 60, 42, 65, 30, 44, 60, 40, 43, 21, 3, 27, 59, 34, 39, 34, 17, 25, 0, 51, 34, 31, 12, 35, 35, 56, 14, 21, 7, 61, 21, 24, 7, 39, 32, 27, 38, 39, 40, 66, 9, 21, 17, 24, 53, 59, 54, 23, 15, 26, 12, 16, 1, 58, 11, 34, 50, 44, 66, 20, 21, 28, 64, 13, 0, 42, 39, 59, 60, 46, 5, 53, 63, 60, 49, 28, 39, 52, 29, 65, 0, 29, 56, 43, 61, 14, 17, 9, 31, 16, 45, 66, 35, 22, 3, 34, 3, 14, 33, 47, 48, 32, 11, 37, 25, 6, 0, 48, 4, 11, 59, 0, 28, 6, 62, 65, 66, 6, 66, 57, 4, 47, 51, 0, 65, 21, 27, 62, 34, 33, 30, 45, 8, 6, 55, 14, 63, 61, 57, 22, 45, 39, 36, 4, 49, 57, 20, 35, 53, 64, 16, 3, 53, 29, 15, 14, 43, 46, 63, 0, 19, 55, 49, 45, 4, 54, 55, 20, 5, 19, 45, 61, 4, 14, 28, 51, 57, 26, 63, 6, 29, 33, 49, 63, 7, 50, 21, 55, 10, 37, 59, 23, 44, 12, 18, 39, 54, 41, 52, 63, 22, 22, 65, 48, 7, 14, 53, 11, 27, 54, 49, 14, 29, 20, 14, 4, 58, 28, 18, 46, 20, 55, 21, 28, 27, 21, 19, 31, 27, 34, 27, 19, 21, 61, 39, 0, 43, 4, 51, 65, 42, 41, 62, 63, 56, 30, 31, 24, 5];
+//let VspecContSR = [0, 62, 6, 65, 7, 18, 32, 49, 10, 17, 63, 55, 57, 23, 21, 53, 53, 39, 33, 56, 58, 25, 11, 48, 37, 34, 62, 59, 40, 64, 55, 46, 62, 61, 14, 57, 12, 27, 65, 0, 57, 65, 30, 35, 41, 53, 14, 62, 4, 55, 46, 19, 53, 65, 60, 42, 65, 30, 44, 60, 40, 43, 21, 3, 27, 59, 34, 39, 34, 17, 25, 0, 51, 34, 31, 12, 35, 35, 56, 14, 21, 7, 61, 21, 24, 7, 39, 32, 27, 38, 39, 40, 66, 9, 21, 17, 24, 53, 59, 54, 23, 15, 26, 12, 16, 1, 58, 11, 34, 50, 44, 66, 20, 21, 28, 64, 13, 0, 42, 39, 59, 60, 46, 5, 53, 63, 60, 49, 28, 39, 52, 29, 65, 0, 29, 56, 43, 61, 14, 17, 9, 31, 16, 45, 66, 35, 22, 3, 34, 3, 14, 33, 47, 48, 32, 11, 37, 25, 6, 0, 48, 4, 11, 59, 0, 28, 6, 62, 65, 66, 6, 66, 57, 4, 47, 51, 0, 65, 21, 27, 62, 34, 33, 30, 45, 8, 6, 55, 14, 63, 61, 57, 22, 45, 39, 36, 4, 49, 57, 20, 35, 53, 64, 16, 3, 53, 29, 15, 14, 43, 46, 63, 0, 19, 55, 49, 45, 4, 54, 55, 20, 5, 19, 45, 61, 4, 14, 28, 51, 57, 26, 63, 6, 29, 33, 49, 63, 7, 50, 21, 55, 10, 37, 59, 23, 44, 12, 18, 39, 54, 41, 52, 63, 22, 22, 65, 48, 7, 14, 53, 11, 27, 54, 49, 14, 29, 20, 14, 4, 58, 28, 18, 46, 20, 55, 21, 28, 27, 21, 19, 31, 27, 34, 27, 19, 21, 61, 39, 0, 43, 4, 51, 65, 42, 41, 62, 63, 56, 30, 31, 24, 5];
 //Demonstrate all cases
-let VspecContSR = [0, 11, 6, 9, 3, 0, 1, 6, 9, 22, 9, 6, 1, 0, 1, 6, 9, 6, 9, 6, 9, 6, 9, 6, 1, 0, 1, 6, 9, 11, 9, 22, 9, 33, 9, 66, 9, 99, 9, 6, 1, 0, 1, 6, 9, 9, 9, 9, 9, 6, 1, 0, 1, 6, 9, 9, 9, 6, 1, 6, 33, 33, 33, 6, 1, 0, 1, 3, 6, 9, 66, 9, 6, 3, 1, 3, 6, 9, 6, 3, 1, 0, 1, 6, 9, 6, 9, 6, 9, 6, 9, 6, 66, 9, 6, 1, 0, 1, 6, 9, 6, 6, 6, 6, 6, 6, 6, 66, 6, 1, 0, 3, 6, 9, 6, 9, 6, 9, 6, 3, 0, 3, 6, 33, 3, 31, 4, 29, 5, 27, 6, 25, 7, 23, 8, 21, 9, 19, 10, 17, 11, 15, 12, 13, 6, 3, 0, 3, 6, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 9, 6, 3];
+let VspecContSR = [2, 11, 6, 9, 3, 0, 1, 6, 9, 22, 9, 6, 1, 0, 1, 6, 9, 6, 9, 6, 9, 6, 9, 6, 1, 0, 1, 6, 9, 11, 9, 22, 9, 33, 9, 66, 9, 99, 9, 6, 1, 0, 1, 6, 9, 9, 9, 9, 9, 6, 1, 0, 1, 6, 9, 9, 9, 6, 1, 6, 33, 33, 33, 6, 1, 0, 1, 3, 6, 9, 66, 9, 6, 3, 1, 3, 6, 9, 6, 3, 1, 0, 1, 6, 9, 6, 9, 6, 9, 6, 9, 6, 66, 9, 6, 1, 0, 1, 6, 9, 6, 6, 6, 6, 6, 6, 6, 66, 6, 1, 0, 3, 6, 9, 6, 9, 6, 9, 6, 3, 0, 3, 6, 33, 3, 31, 4, 29, 5, 27, 6, 25, 7, 23, 8, 21, 9, 19, 10, 17, 11, 15, 12, 13, 6, 3, 0, 3, 6, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 9, 6, 3];
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
@@ -48,11 +48,9 @@ let VspecContSRcloudCalcsM = [
                              ];
 
 //Combined calculation
-let VspecContSRcloudCalcCombinedA = [new Array(CycIteration).fill(0)];
-let VspecContSRcloudCalcCombinedP = 0;
-let VspecContSRcloudCalcCombinedT = 0;
-let VspecContSRcloudCalcCombinedB = 0;
-let VspecContSRcloudCalcCombinedE = 0;
+let VspecContSRcloudCalcCombinedA = [new Array(CycIteration).fill(0)];  //Combined array
+let VspecContSRcloudCalcCombinedP = 0;                                  //Peak
+let VspecContSRcloudCalcCombinedT = 0;                                  //Trough
 
 //Peaks
 let VspecContSRcloudTempPeaksCount = 0;
@@ -78,7 +76,6 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //TODO - Zero index edge case: if spectrum index zero is as a peak, it will get missed (could default index 0 to always be zero?)
-//     - Combined fails if array doesn't start with a zero
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             
             //Clear SR Clouds matrix to nulls
@@ -111,11 +108,12 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
             ///////////////////////////////////////////////
             ///////////Begin peak identifications///////////
             i = 0;
-            while(i < (CycIteration + 1))
+            while(i < CycIteration)
                  {
                   ///////////BASE BEGIN///////////
                   //Identify peak base begin (undefined for before the spectrum) (i-1 < 0 for before the spectrum)
                   if((VspecContSR[i-1] == 0 || i-1 < 0) && VspecContSR[i] > 0)
+                  //if(VspecContSR[i-1] == 0 && VspecContSR[i] > 0)
                     {
                      //Clear temporary SR cloud matrix to nulls
                      j = 0; 
@@ -140,6 +138,10 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
                      VspecContSRcloudTempTroughsAccume = 0;
                      VspecContSRcloudCalcsTroughsWeighted = 0;        
                      VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
+                     
+                     //Clear combined
+                     VspecContSRcloudCalcCombinedP = 0;
+                     VspecContSRcloudCalcCombinedT = 0;
                      
                     //Push cloud base begins
                      j = 0; 
@@ -379,7 +381,7 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
                           {
                            VspecContSRcloudCalcsPeaksWeighted += VspecContSRcloudTempM[1][j] * VspecContSRcloudTempM[0][j];  //Multiply peak value with index value, then add to 'VspecContSRcloudCalcsPeaksWeighted'
                            VspecContSRcloudCalcsPeaksWeightedDivisor += VspecContSRcloudTempM[1][j];                   //Add peak values together into 'VspecContSRcloudCalcsPeaksWeightedDivisor'
-                              
+                          
                            j++;
                           }
                      
@@ -391,6 +393,8 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
                              {
                               //Divide results of peak average center weighted index and push
                               VspecContSRcloudCalcsM[1][j] = Math.round(VspecContSRcloudCalcsPeaksWeighted / VspecContSRcloudCalcsPeaksWeightedDivisor);
+                              VspecContSRcloudCalcCombinedP = VspecContSRcloudCalcsM[1][j];
+                              
                               break;
                              }
                            else j++;
@@ -433,70 +437,24 @@ let VspecContSRcloudCalcsTroughsWeightedDivisor = 0;
                              {
                               //Divide results of trough average center weighted index and push
                               VspecContSRcloudCalcsM[3][j] = Math.round(VspecContSRcloudCalcsTroughsWeighted / VspecContSRcloudCalcsTroughsWeightedDivisor);
+                              VspecContSRcloudCalcCombinedT = VspecContSRcloudCalcsM[3][j];
+                              
                               break;
                              }
                            else j++;
                           }
                      
                      ///////////COMBINED///////////
-                     //Combine weighted peak and trough to get 'true' weighted average
-                     VspecContSRcloudCalcCombinedP = 0;
-                     VspecContSRcloudCalcCombinedT = 0;
-                    
+                     //Commbine weighted peak and trough, and push
                      j = 0;
-                     while(true)
+                     while(true) 
                           {
-                           //Go to end of begins
-                           if(!VspecContSRcloudsM[0][j])
+                           if(!VspecContSRcloudCalcCombinedA[j]) 
                              {
-                              //Current cloud begin and end points
-                              VspecContSRcloudCalcCombinedB = VspecContSRcloudsM[0][j-1];
-                              VspecContSRcloudCalcCombinedE = VspecContSRcloudsM[3][j-1];
-                              
-                              //Check all weighted peaks
-                              k = 0;
-                              while(VspecContSRcloudCalcsM[1][k])
-                                   {
-                                    l = VspecContSRcloudCalcsM[1][k];
-                                    
-                                    if(l >= VspecContSRcloudCalcCombinedB && l <= VspecContSRcloudCalcCombinedE)
-                                      {
-                                       VspecContSRcloudCalcCombinedP = VspecContSRcloudCalcsM[1][k];
-                                       break;
-                                      }
-                                    else k++;
-                                   }
-                              
-                              //Check all weighted troughs
-                              VspecContSRcloudCalcCombinedT = 0;
-                              k = 0;
-                              while(VspecContSRcloudCalcsM[3][k])
-                                   {
-                                    l = VspecContSRcloudCalcsM[3][k];
-                                    
-                                    if(l >= VspecContSRcloudCalcCombinedB && l <= VspecContSRcloudCalcCombinedE)
-                                      {
-                                       VspecContSRcloudCalcCombinedT = VspecContSRcloudCalcsM[3][k];
-                                       break;
-                                      }
-                                    else k++;
-                                   }
-                              
-                              //Commbine weighted peak and trough, and push
-                              k = 0;
-                              while(true) 
-                                   {
-                                    if(!VspecContSRcloudCalcCombinedA[k]) 
-                                      {
-                                       //Combine weighted results and average
-                                       if(VspecContSRcloudCalcCombinedT) VspecContSRcloudCalcCombinedA[k] = Math.round((VspecContSRcloudCalcCombinedP + VspecContSRcloudCalcCombinedT) / 2);
-                                       else VspecContSRcloudCalcCombinedA[k] = VspecContSRcloudCalcCombinedP;   //In case no trough
-                                       
-                                       break;
-                                      }
-                                    else k++;
-                                   }
-                              
+                              //Combine weighted results and average
+                              if(VspecContSRcloudCalcCombinedP && VspecContSRcloudCalcCombinedT) VspecContSRcloudCalcCombinedA[j] = Math.round((VspecContSRcloudCalcCombinedP + VspecContSRcloudCalcCombinedT) / 2);
+                              if(VspecContSRcloudCalcCombinedP &! VspecContSRcloudCalcCombinedT) VspecContSRcloudCalcCombinedA[j] = VspecContSRcloudCalcCombinedP;  //When no trough
+                               
                               break;
                              }
                            else j++;
@@ -527,40 +485,46 @@ let outputString = "",
 //Create points for visual spectrum representation
 for(i = 0; VspecContSR.length > i; i++)
    {
-    
-    /*
+    ///*
     //Cloud base begins
     j = 0;
     while(VspecContSRcloudsM[0].length > j)
          {
-          if(i == VspecContSRcloudsM[0][j]) baseBeginString = `      <--- Base begin (${VspecContSR[i]}) - Index ${i}`;   
+          if(i == VspecContSRcloudsM[0][j]) baseBeginString = `      <-- Base begin (${VspecContSR[i]}) - Index ${i}`;   
           j++;
          }
-         
+    //*/
+     
+    /*
     //Cloud peaks     
     j = 0;
     while(VspecContSRcloudsM[1].length > j)
          {
-          if(i == VspecContSRcloudsM[1][j]) peakString = `     <--- Peak (${VspecContSR[i]}) - Index ${i}`;   
+          if(i == VspecContSRcloudsM[1][j]) peakString = `     <-- Peak (${VspecContSR[i]}) - Index ${i}`;   
           j++;
          }
+    */
     
+    /*
     //Cloud troughs 
     j = 0;
     while(VspecContSRcloudsM[2].length > j)
          {
-          if(i == VspecContSRcloudsM[2][j]) troughString = `     <--- Trough (${VspecContSR[i]}) - Index ${i}`;   
+          if(i == VspecContSRcloudsM[2][j]) troughString = `     <-- Trough (${VspecContSR[i]}) - Index ${i}`;   
           j++;
          }
+    */
     
+    ///*
     //Cloud base ends
     j = 0;
     while(VspecContSRcloudsM[3].length > j)
          {
-          if(i == VspecContSRcloudsM[3][j]) baseEndString = `     <--- Base end (${VspecContSR[i]}) - Index ${i}`;   
+          if(i == VspecContSRcloudsM[3][j]) baseEndString = `     <-- Base end (${VspecContSR[i]}) - Index ${i}`;   
           j++;
          }
-    */
+    //*/
+    
     //////
     
     /*
@@ -568,7 +532,7 @@ for(i = 0; VspecContSR.length > i; i++)
     j = 0;
     while(VspecContSRcloudCalcsM[0].length > j)
          {
-          if(i == VspecContSRcloudCalcsM[0][j]) peakCenterStr = `              <=== PEAK average center (${VspecContSR[i]}) - Index ${i}`;
+          if(i == VspecContSRcloudCalcsM[0][j]) peakCenterStr = `      <== PEAK average center (${VspecContSR[i]}) - Index ${i}`;
           j++;
          }
         
@@ -576,7 +540,7 @@ for(i = 0; VspecContSR.length > i; i++)
     j = 0;
     while(VspecContSRcloudCalcsM[2].length > j)
          {
-          if(i == VspecContSRcloudCalcsM[2][j]) troughCenterStr = `              <=== TROUGH average center (${VspecContSR[i]}) - Index ${i}`;
+          if(i == VspecContSRcloudCalcsM[2][j]) troughCenterStr = `      <== TROUGH average center (${VspecContSR[i]}) - Index ${i}`;
           j++;
          }
     */
@@ -585,7 +549,7 @@ for(i = 0; VspecContSR.length > i; i++)
     j = 0;
     while(VspecContSRcloudCalcsM[1].length > j)
          {
-          if(i == VspecContSRcloudCalcsM[1][j]) weightedPeakCenterStr = `              <=== PEAK weighted average center (${VspecContSR[i]}) - Index ${i}`;
+          if(i == VspecContSRcloudCalcsM[1][j]) weightedPeakCenterStr = `      <== PEAK weighted average center (${VspecContSR[i]}) - Index ${i}`;
           j++;
          }
         
@@ -593,7 +557,7 @@ for(i = 0; VspecContSR.length > i; i++)
     j = 0;
     while(VspecContSRcloudCalcsM[3].length > j)
          {
-          if(i == VspecContSRcloudCalcsM[3][j]) weightedTroughCenterStr = `              <=== TROUGH weighted average center (${VspecContSR[i]}) - Index ${i}`;
+          if(i == VspecContSRcloudCalcsM[3][j]) weightedTroughCenterStr = `      <== TROUGH weighted average center (${VspecContSR[i]}) - Index ${i}`;
           j++;
          }
     
@@ -601,7 +565,7 @@ for(i = 0; VspecContSR.length > i; i++)
     j = 0;
     while(VspecContSRcloudCalcCombinedA.length > j)
          {
-          if(i == VspecContSRcloudCalcCombinedA[j]) trueCenter = `              <===---- TRUE CENTER (${VspecContSR[i]}) - Index ${i}`;
+          if(i == VspecContSRcloudCalcCombinedA[j]) trueCenter = `                        <~~~~~~~ TRUE CENTER (${VspecContSR[i]}) - Index ${i}`;
           j++;
          }
     
@@ -631,8 +595,15 @@ for(i = 0; VspecContSR.length > i; i++)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-console.log("\n\n")
+console.log("\n")
 console.log(`Array Length: ${VspecContSR.length}`);
 //
 console.log(`Peaks Average Value: ${VspecContSRcloudTempPeaksAccumeTotal / VspecContSRcloudTempPeaksCount}`);
 console.log(`Troughs Average Value: ${VspecContSRcloudTempTroughsAccumeTotal / VspecContSRcloudTempTroughsCount}`);
+//
+console.log("\n");
+console.log(`VspecContSRcloudsM[0]: ${VspecContSRcloudsM[0]}`);
+console.log(`VspecContSRcloudsM[0]: ${VspecContSRcloudsM[1]}`);
+console.log(`VspecContSRcloudsM[0]: ${VspecContSRcloudsM[2]}`);
+console.log(`VspecContSRcloudsM[0]: ${VspecContSRcloudsM[3]}`);
+console.log(`VspecContSRcloudCalcCombinedA: ${VspecContSRcloudCalcCombinedA}`);
